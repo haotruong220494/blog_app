15d9a727f67a414223312d3df9ba4e13b142417d
haotruong220494 <truongnhatnam003@gmail.com>
2020-09-25 13:31:18 +0700

    Config deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, :production
 set :branch, "master"
-set :deploy_to, "/home/haotruong/deploy/demo_danghh"
+set :deploy_to, "/home/haotruong/deploy/blog_app"
 server "13.212.124.252", user: "haotruong", roles: %w{app db web}
