46d628dbc00f76b56c4ff611f2a7266180747e36
haotruong220494 <truongnhatnam003@gmail.com>
2020-10-05 15:10:53 +0700

    fix file acticle.js

--- app/assets/javascripts/admin/articles.js
+++ app/assets/javascripts/admin/articles.js
@@ -1,10 +1,10 @@
-$(function() {
-  $(".change-slug").on("click", function(){
-    const valInputTittle = $(".input-title").val();
-    if(valInputTittle != ""){
-      const valSlug = valInputTittle.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/đ/g, "d").replace(/Đ/g, "D").replace(/\s/g, "-");
-      // var valSlug = valInputTittle.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
-      $(".input-slug").val(valSlug);
-    }
-  });
-});
+// $(function() {
+//   $(".change-slug").on("click", function(){
+//     const valInputTittle = $(".input-title").val();
+//     if(valInputTittle != ""){
+//       const valSlug = valInputTittle.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/đ/g, "d").replace(/Đ/g, "D").replace(/\s/g, "-");
+//       // var valSlug = valInputTittle.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+//       $(".input-slug").val(valSlug);
+//     }
+//   });
+// });
