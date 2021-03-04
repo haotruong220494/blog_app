8add4094ecca314d2285a3de42c14ac3d2260c51
haotruong220494 <truongnhatnam003@gmail.com>
2021-03-04 16:34:51 +0700

    config deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, :production
 set :branch, "master"
 set :deploy_to, "/var/www/blog_app"
-server "54.169.118.131", :user => "ubuntu", :roles => %w(app db web)
+server "18.141.99.75", :user => "ubuntu", :roles => %w(app db web)
