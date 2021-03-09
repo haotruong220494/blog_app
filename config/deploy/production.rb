dc16051d5d5b9f23d081e0e308abaa33c594e12e
haotruong220494 <truongnhatnam003@gmail.com>
2021-03-09 13:58:29 +0700

    config deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, :production
 set :branch, "master"
 set :deploy_to, "/var/www/blog_app"
-server "46.137.215.40", :user => "ubuntu", :roles => %w(app db web)
+server "54.179.60.241", :user => "ubuntu", :roles => %w(app db web)
