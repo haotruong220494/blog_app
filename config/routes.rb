d8ffabc2948a2dca795770ddab431333c9e837fa
haotruong220494 <truongnhatnam003@gmail.com>
2021-03-11 11:52:42 +0700

    config page 404

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,7 @@ Rails.application.routes.draw do
   end
   get "/sitemap.xml", to: "sitemap#index", defaults: {format: "xml"}
   get "/robots.txt", to: "robots_txts#index"
+  match "/404", to: "errors#file_not_found", via: :all
+  match "/422", to: "errors#unprocessable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
